972ce2170f083cefb44138c7e285abf3a35ee8b5
liukai <liukai9106@163.com>
2017-05-25 15:31:43 +0800

    initial commit

---
+++ Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime
+//
+//  Created by liukai on 25/05/2017.
+//  Copyright © 2017 com.gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime
+//
+//  Created by liukai on 25/05/2017.
+//  Copyright © 2017 com.gavin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
